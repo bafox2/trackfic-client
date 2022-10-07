c83d3013ac976a2486a9c2784be4e04177edfce7
ben <benjaminalexanderfox11@gmail.com>
2022-10-07 11:39:18 -0400

    starting mantine layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { MantineProvider, Text, Button, Stack } from "@mantine/core";
+import {
+	MantineProvider,
+	Text,
+	Button,
+	Stack,
+	AppShell,
+	Navbar,
+	Header,
+	Footer,
+} from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
 	return (
-		<MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-			<Stack align="flex-end">
-				<Text size="xs">Hello world</Text>
-			</Stack>
-		</MantineProvider>
+		<AppShell themeOverride={theme}>
+			padding="md"
+			// navbar={
+			// 	<Navbar width={{ base: 300 }} height={500} p="xs">
+			// 		{/* Navbar content */}
+			// 	</Navbar>
+			// }
+			header={
+				<Header height={60} p="xs">
+					{/* Header content */}
+				</Header>
+			}
+			footer={<Footer height={30}>{/* Footer content */}</Footer>}
+		>
+			{/* Your application here */}
+			<MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+				<Stack align="flex-end">
+					<Text size="xs">Hello world</Text>
+				</Stack>
+			</MantineProvider>
+		</AppShell>
 	);
 }
