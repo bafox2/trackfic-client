ad186ae80f179d37f9ce675714307ebe8cf14f00
ben <benjaminalexanderfox11@gmail.com>
2022-11-05 19:21:44 -0400

    styling the table

--- Components/AccordionHeader/AccordionHeader.tsx
+++ Components/AccordionHeader/AccordionHeader.tsx
@@ -1,49 +1,6 @@
 import { Text, Stack, createStyles, Paper } from "@mantine/core";
 
-const useStyles = createStyles((theme) => ({
-	header: {
-		position: "sticky",
-		top: 0,
-		backgroundColor:
-			theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-		transition: "box-shadow 150ms ease",
-
-		"&::after": {
-			content: '""',
-			position: "absolute",
-			left: 0,
-			right: 0,
-			bottom: 0,
-			borderBottom: `1px solid ${
-				theme.colorScheme === "dark"
-					? theme.colors.dark[3]
-					: theme.colors.gray[2]
-			}`,
-		},
-	},
-
-	th: {
-		padding: "0 !important",
-	},
-
-	control: {
-		width: "100%",
-		padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
-
-		"&:hover": {
-			backgroundColor:
-				theme.colorScheme === "dark"
-					? theme.colors.dark[6]
-					: theme.colors.gray[0],
-		},
-	},
-
-	icon: {
-		width: 21,
-		height: 21,
-		borderRadius: 21,
-	},
-}));
+const useStyles = createStyles((theme) => ({}));
 
 interface AccordionHeaderProps {
 	data: {
@@ -59,6 +16,7 @@ interface AccordionHeaderProps {
 }
 
 export default function AccordionHeader({ data }: AccordionHeaderProps) {
+	const { classes } = useStyles();
 	return (
 		<>
 			<Stack align="flex-start" justify="center" spacing="xs">
