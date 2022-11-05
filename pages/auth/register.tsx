d8150f2ef26dcc2d6a0271027bc3c19809921453
ben <benjaminalexanderfox11@gmail.com>
2022-11-05 19:25:25 -0400

    register image changed

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -43,7 +43,7 @@ type CreateUserInput = TypeOf<typeof createUserSchema>;
 
 const useStyles = createStyles((theme) => ({
 	wrapperForm: {
-		minHeight: 900,
+		minHeight: 800,
 		backgroundSize: "cover",
 		backgroundImage:
 			"url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)",
@@ -53,7 +53,7 @@ const useStyles = createStyles((theme) => ({
 		borderRight: `1px solid ${
 			theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
 		}`,
-		minHeight: 900,
+		minHeight: 800,
 		maxWidth: 450,
 		paddingTop: 80,
 
